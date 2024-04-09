208c858ce4db7299a6607722ee06c50fe0fc127f
Kevin Yee <kevinyee@Kevins-MacBook-Pro.local>
2024-04-09 15:04:03 -0700

    Fix the 404 navigation error by using HashRouter instead of BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from './pages/HomePage.tsx';
 import { About } from './pages/About.tsx';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
