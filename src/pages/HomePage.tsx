dd5d074db2e98008d3358303853c5488066c7314
Kevin Yee <kevinyee@Kevins-MacBook-Pro.local>
2024-04-09 15:48:52 -0700

    Set up CSS tailwind

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,6 +5,10 @@ export const HomePage = () => {
     return (
         <div>
             hello!! i am the home page
+
+            <header className="text-5xl font-bold underline">
+                Hello Tailwind!
+            </header>
             <Link to='/about'>Go to about!</Link>
         </div>
     )
